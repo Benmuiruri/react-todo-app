b8c64236cd8d3c52d11813de6e887e92d565da78
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-25 17:05:51 +0300

    Finish page setup

---
+++ src/functionBased/components/TodoItem.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import styles from './TodoItem.module.css';
+import { FaTrash } from 'react-icons/fa';
+
+const TodoItem = (props) => {
+  const [editing, setEditing] = useState(false);
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const handleUpdatedDone = (e) => {
+    if (e.key === 'Enter') {
+      setEditing(false);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      console.log('Cleaning up...');
+    };
+  }, []);
+
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
+  const { completed, id, title } = props.todo;
+
+  let viewMode = {};
+  let editMode = {};
+
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
+        <input
+          type='checkbox'
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
+        />
+        <button onClick={() => props.deleteTodoProps(id)}>
+          <FaTrash style={{ color: 'orangered', fontSize: '16px' }} />
+        </button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
+      <input
+        type='text'
+        value={title}
+        className={styles.textInput}
+        style={editMode}
+        onChange={(e) => {
+          props.updateItem(e.target.value, id);
+        }}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
+
+export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import TodoContainer from './functionBased/components/TodoContainer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './functionBased/components/TodoContainer';
+import { BrowserRouter } from 'react-router-dom';
 
 import './functionBased/App.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
